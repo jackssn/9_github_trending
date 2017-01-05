b5a96e2c6962b8a6a898c9211ad5c20820365c5d
Jackssn <mtjeck@gmail.com>
2017-01-05 13:54:50 +0400

    change var names and improve readability

--- github_trending.py
+++ github_trending.py
@@ -2,21 +2,22 @@ import requests
 from datetime import datetime, timedelta
 
 
-def get_trending_repositories(top_size):
-    week_ago = datetime.strftime(datetime.now() - timedelta(days=7), '%Y-%m-%d')
+def get_trending_repositories(top_size, days_ago):
+    start_date = datetime.strftime(datetime.now() - timedelta(days=days_ago), '%Y-%m-%d')
     url = 'https://api.github.com/search/repositories'
-    payload = {'q': 'created:>%s' % week_ago, 'sort': 'stars', 'per_page': top_size}
+    payload = {'q': 'created:>%s' % start_date, 'sort': 'stars', 'per_page': top_size}
     response = requests.get(url, params=payload)
     return response.json()["items"]
 
 
 def print_repositories(data):
-    for i, repository in enumerate(data):
-        print('%s) Repository "%s"' % (i + 1, repository['name']),
+    for number, repository in enumerate(data, start=1):
+        print('%s) Repository "%s"' % (number, repository['name']),
               'has %s stars, %s open issues' % (repository['stargazers_count'], repository['open_issues']),
               'and located here: %s' % repository['html_url'])
 
 
 if __name__ == '__main__':
     repositories_quantity = 20
-    print_repositories(get_trending_repositories(repositories_quantity))
+    period_of_days = 7
+    print_repositories(get_trending_repositories(repositories_quantity, period_of_days))
